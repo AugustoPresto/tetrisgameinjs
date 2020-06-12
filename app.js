c631435186a4c72ae1d499c3d399a1c9712df383
Augusto Souza <augustosouza2222@gmail.com>
2020-06-11 22:41:00 -0300

    feito até tetrominos

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+
+  const width = 10
+
+})
+
+
+/* https://www.youtube.com/watch?v=rAUn1Lom6dw - parei em drawing tetrominoes*/
